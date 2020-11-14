5e9019516f35ade3e1227c0f562369bdc1b82292
slime_turtle <61219276+turtle85917@users.noreply.github.com>
2020-11-14 11:18:01 +0900

    Update pingpong.js

--- commands/기본명령어/pingpong.js
+++ commands/기본명령어/pingpong.js
@@ -4,6 +4,6 @@ module.exports = {
     category: '기본명령어',
     run: async (client, message, args) => {
         if (!args.join(' ')) return message.channel.send("아무것도 없네요");
-        require("node-fetch")(`https://builder.pingpong.us/api/builder/5e7c9eeae4b03219076c4844/integration/v0.2/custom/${message.author.id}`, {method: "POST", headers: {"Authorization": `Basic a2V5OmIyYjRkMTYwYTk4NmI3MzEwMTE5NGEyMDI1NjkyMjcz`, "Content-Type": "application/json"}, body: JSON.stringify({request: {query: args.join(' ')}})}).then(r => r.json()).then(({ response: { replies: [{ text }] } }) => message.channel.send(text))
+        require("node-fetch")(`https://builder.pingpong.us/api/builder/${yourkey}/integration/v0.2/custom/${message.author.id}`, {method: "POST", headers: {"Authorization": `Basic ${basickey}`, "Content-Type": "application/json"}, body: JSON.stringify({request: {query: args.join(' ')}})}).then(r => r.json()).then(({ response: { replies: [{ text }] } }) => message.channel.send(text))
     }
 }
